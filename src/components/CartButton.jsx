ef685ea10d74510fab5e00cb818894fca321c65f
automation.andy101 <automation.andy101@gmail.com>
2025-08-27 22:39:03 +0100

    first commit

---
+++ src/components/CartButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useCart } from '../context/cartContext'
+
+const CartButton = ({ item }) => {
+    const { addToCart } = useCart()
+
+    return (
+        <div className='w-max absolute right-5 top-5'>
+            <div className='space-x-3'>
+                <button
+                    type="button"
+                    className='bg-zinc-400 border rounded-md px-2 py-1 text-sm text-white hover:bg-zinc-500 transition-colors'
+                    onClick={() => addToCart(item)}    
+                >
+                    + Add to cart
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default CartButton
---
+++ src/context/cartContext.jsx
@@ -0,0 +1,34 @@
+import { allProducts } from "../assets/data";
+import { createContext, useContext, useState } from 'react'
+
+const CartContext = createContext()
+
+export const CartProvider = ({ children }) => {
+    const [allItems, setAllItems] = useState([])
+
+    const setItems = () => {
+        setAllItems(allProducts)
+    }
+
+    const addToCart = (item) => {
+        setAllItems((prevItems) => {
+            return prevItems.map((prevItem) => {
+                if (prevItem.inCart) {
+                    return prevItem
+                }
+
+                return prevItem.id === item.id ? { ...prevItem, inCart: true } : prevItem
+            })
+        })
+    }
+
+    return (
+        <CartContext.Provider value={{ allItems, setItems, addToCart }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export const useCart = () => {
+    return useContext(CartContext)
+}
