db1ebc5c460c1795342afba343b929ae5ddba8ae
automation.andy101 <automation.andy101@gmail.com>
2025-09-30 17:00:21 +0100

    Added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ const App = () => {
 
   return (
     <div className="grid place-items-center py-20">
-      <h1 className="text-5xl italic text-gray-500 mb-16">
+      <h1 className="lg:text-5xl md:text-4x text-3xl italic text-gray-500 mb-16 px-10 text-center">
         Trend Alert: Must-Have Outfits of the Season
       </h1>
       <ShoppingCart />
-      <div className="grid grid-cols-3 place-items-start gap-10">
+      <div className="grid xl:grid-cols-3 lg:grid-cols-2 place-items-start gap-10 xl:px-6 px-10">
         {allItems?.map((item) => {
           return (
             <CartItem key={item.id} item={item} />
